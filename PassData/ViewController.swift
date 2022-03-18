30cc179bd35c3111337d59b86ba1d01edfb6665d
sy2298 <sy2298@naver.com>
2022-03-18 21:43:56 +0900

    Initial Commit

---
+++ PassData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassData
+//
+//  Created by 이서영 on 2022/03/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
