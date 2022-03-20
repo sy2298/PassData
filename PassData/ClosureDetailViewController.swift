afd247822ec453ff44b896cbec67df3f9d620fd4
sy2298 <sy2298@naver.com>
2022-03-20 16:37:46 +0900

    Closure Pattern

---
+++ PassData/ClosureDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ClosureDetailViewController.swift
+//  PassData
+//
+//  Created by 이서영 on 2022/03/19.
+//
+
+import UIKit
+
+class ClosureDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
